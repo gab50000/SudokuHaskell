d0335cffbbe9c30a2581cf6e16419d51014b4c14
Gabriel Kabbe <gabriel.kabbe@mail.de>
2020-10-09 21:31:02 +0200

    Add function which check for Sudoku completeness

--- sudoku.hs
+++ sudoku.hs
@@ -1,4 +1,15 @@
 empty_sudoku = take 9 $ repeat $ take 9 $ repeat 0
 
+is_completed :: [[Int]] -> Bool
+is_completed (row : rest) = is_completed_row row && is_completed rest
+
+is_completed_row :: [Int] -> Bool
+is_completed_row (x : xs) = x /= 0 && is_completed_row xs
+
+addElem :: Int -> [[Int]] -> [[Int]]
+addElem num sudoku
+  | is_completed sudoku = sudoku
+  | otherwise = [[100]]
+
 main = do
-  print empty_sudoku
+  print $ is_completed empty_sudoku
